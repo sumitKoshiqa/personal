b7a06858957e8b5d9dd0559044740397d05e36f6
ashwani <ashwani.janu@gmail.com>
2023-08-18 14:58:42 +0530

    TextField Fix 11

--- lib/Consumer/Profile/Networking/ProfileApi.dart
+++ lib/Consumer/Profile/Networking/ProfileApi.dart
@@ -64,7 +64,7 @@ class ProfileApi {
         return null;
       }
     } catch (e) {
-      print("Exception occurred get profile $e");
+      print("Exception occurred get other profile $e");
       return null;
     }
   }
